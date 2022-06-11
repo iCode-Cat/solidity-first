32d9e4524b6420bd1d8d7c855e3b1662bcf1b7b9
iCode-Cat <devranboyac8@gmail.com>
2022-06-11 15:09:15 +0300

    initial

---
+++ compile.js
@@ -0,0 +1,10 @@
+// compile code will go here
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+const readFile = fs.readFileSync(inboxPath, 'utf8');
+
+// left number amount of contracs!
+module.exports = solc.compile(readFile, 1).contracts[':Inbox'];
